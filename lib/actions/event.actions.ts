552137357de3811cff2f3d13ad0c97a2a129d340
Godwin <gyamfiadu01@gmail.com>
2024-01-25 18:07:25 +0000

    changes in event actions file

--- lib/actions/event.actions.ts
+++ lib/actions/event.actions.ts
@@ -35,7 +35,7 @@ export async function createEvent({ userId, event, path }: CreateEventParams) {
     const organizer = await User.findOne({clerkId:userId})
     if (!organizer) throw new Error('Organizer not found')
 
-    const newEvent = await Event.create({ ...event, category: event.categoryId, organizer: userId })
+    const newEvent = await Event.create({ ...event, category: event.categoryId, organizer: organizer?._id })
     revalidatePath(path)
 
     return JSON.parse(JSON.stringify(newEvent))
